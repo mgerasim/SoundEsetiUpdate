3b23a7da5cad930e7c691b5acb9ed5e89fa1ff0f
Mikhail Gerasimov <mgerasim@inbox.ru>
2022-11-02 10:09:06 +0300

    dotnet-install

---
+++ install.sh
@@ -0,0 +1,2 @@
+sudo chmod +x ./dotnet-install.sh
+./dotnet-install.sh -c 5.0
--- run.sh
+++ run.sh
@@ -6,4 +6,8 @@ echo Запускает обновление $RELEASE_VERSION >> $LOG &>> $LOG
 
 cd /home/slay/updates/SoundEsetiUpdate # >> $LOG &>> $LOG
 #git pull origin master >> $LOG &>> $LOG
+
+
+sh ./install.sh
+
 sh /home/slay/updates/SoundEsetiUpdate/update.sh $LOG $RELEASE_VERSION
